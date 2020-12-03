a42fcad1feefa91f992c4ff80c67647c88adba54
DaniyalAhmadKhan-LUMS <22100215@lums.edu.pk>
2020-12-03 11:13:55 +0500

    1stFinal

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -5,11 +5,13 @@ public class Events : MonoBehaviour
 {
   public void ReplayGame()
   {
+      
       SceneManager.LoadScene("Level1");
 
   }
   public void QuitGame()
   {
+      
       Application.Quit();
   }
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
