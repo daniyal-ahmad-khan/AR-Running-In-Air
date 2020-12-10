9bdd844b00cde61ff59036bac772f80fd2976c50
Muhammad Sameer <21100171@lums.edu.pk>
2020-12-10 22:37:07 +0500

    added scene transition and tougher spawns

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
     private Vector3 offSet;
+    private int scale = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +15,10 @@ public class CameraController : MonoBehaviour
 
     // Update is called once per frame
     void LateUpdate()
-    {
+    {       
         Vector3 newPosition = new Vector3(transform.position.x,transform.position.y,offSet.z+target.position.z);
         transform.position = Vector3.Lerp(transform.position,newPosition,100*Time.deltaTime);
+
+
     }
 }
